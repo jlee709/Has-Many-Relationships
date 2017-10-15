42efebab6076a87104311a1f472fe63b84feed7e
Justin Lee <leejwg.3@gmail.com>
2017-10-14 17:31:48 -1000

    most queries written but needs some refactoring

--- joins.sql
+++ joins.sql
@@ -10,34 +10,72 @@ INNER JOIN posts ON posts.users_id = users_id
 WHERE users_id = 200;
 
 -- 1. Create a query to get all posts fields, and the user's username, from the `posts` table where the user's first name is 'Norene' and the user's lastame is 'Schmitt'
-SELECT posts *, users.username FROM users
+SELECT posts * users.username FROM users
 INNER JOIN posts ON posts.users_id = users.id
 WHERE first_name = "Norene" AND lastame = "Schmitt";
 
 -- 1. Create a query to get usernames from the `users` table where the user has created a post after January 1, 2015
 SELECT users.username FROM users
 INNER JOIN posts ON posts.users_id = users_id
-WHERE posts.created_at > "January 1, 2015";
+WHERE posts.created_at > 'January 1, 2015';
 
 
 -- 1. Create a query to get the post title, post content, and user's username where the user who created the post joined before January 1, 2015
 SELECT posts.title AS post_title, posts.content AS post_content
 users.username AS user_username, users.created_at FROM users
-INNER JOIN posts ON posts.users_id = users.id WHERE users.created_at < "January 1, 2015";
+INNER JOIN posts ON posts.users_id = users.id WHERE users.created_at < 'January 1, 2015';
 
--- 1. Create a query to get the all rows in the `comments` table, showing post title (aliased as 'Post Title'), and the all the comment's fields
-SELECT COMMENTS * posts.title AS post_title FROM posts
-INNER JOIN comments ON comments.posts_id = posts.id;
+-- 1. Create a query to get the all rows in the `comments` table, showing post title 
+-- (aliased as 'Post Title'), and the all the comment's fields
 
+SELECT *, posts.title AS post_title FROM comments
+INNER JOIN posts ON comments.posts_id = posts.id;
 
+-- 1. Create a query to get the all rows in the `comments` table, showing post title (aliased as post_title),
+-- post url (ailased as post_url), and the comment body (aliased as comment_body) where the post was created 
+-- before January 1, 2015
 
--- 1. Create a query to get the all rows in the `comments` table, showing post title (aliased as post_title), post url (ailased as post_url), and the comment body (aliased as comment_body) where the post was created before January 1, 2015
--- 1. Create a query to get the all rows in the `comments` table, showing post title (aliased as post_title), post url (ailased as post_url), and the comment body (aliased as comment_body) where the post was created after January 1, 2015
+SELECT posts.title AS post_title, posts.url AS post_url
+posts.created_at AS post_created_at, comments.body AS comment_body FROM posts
+INNER JOIN comments ON comments.posts_id = posts.id
+WHERE posts.created_at < 'January 1, 2015';
+
+-- 9. Create a query to get the all rows in the `comments` table, showing post title (aliased as post_title), post url (ailased as post_url), and the comment body (aliased as comment_body) where the post was created after January 1, 2015
+SELECT posts.title AS post_title posts.url AS post_url
+posts.created_at AS post_Created_at comments.body AS comment_body FROM posts
+INNER JOIN comments ON comments.posts_id = posts.id
+Where posts.created_at > 'January 1 ,2015';
+
 -- 1. Create a query to get the all rows in the `comments` table, showing post title (aliased as post_title), post url (ailased as post_url), and the comment body (aliased as comment_body) where the comment body contains the word 'USB'
--- 1. Create a query to get the post title (aliased as post_title), first name of the author of the post, last name of the author of the post, and comment body (aliased to comment_body), where the comment body contains the word 'matrix' ( should have 855 results )
--- 1. Create a query to get the first name of the author of the comment, last name of the author of the comment, and comment body (aliased to comment_body), where the comment body contains the word 'SSL' and the post content contains the word 'dolorum' ( should have 102 results )
--- 1. Create a query to get the first name of the author of the post (aliased to post_author_first_name), last name of the author of the post (aliased to post_author_last_name), the post title (aliased to post_title), username of the author of the comment (aliased to comment_author_username), and comment body (aliased to comment_body), where the comment body contains the word 'SSL' or 'firewall' and the post content contains the word 'nemo' ( should have 218 results )
+SELECT posts.title AS post_title, posts.url AS post_url, comments.body AS comment_body FROM posts
+INNER JOIN comments ON comments.posts_id - posts_id WHERE comments.body LIKE '%USB%';
 
+-- 1. Create a query to get the post title (aliased as post_title), first name of the author of the post, last name of the author of the post, and comment body (aliased to comment_body), where the comment body contains the word 'matrix' ( should have 855 results )
+
+SELECT posts.title AS post_title, users.first_name AS post_suthor_firstname, 
+users.last_name AS post_author_last_name, comments.body AS comment_body FROM users
+INNER JOIN posts ON posts.users_id = user.id 
+  INNER JOIN comments ON comments comments.posts_id = posts.id 
+  WHERE comments.body like '%matrix%';
+
+
+
+-- 1. Create a query to get the first name of the author of the comment, last name of the author of the comment, and comment body (aliased to comment_body), where the comment body contains the word 'SSL' and the post content contains the word 'dolorum' ( should have 102 results )
+SELECT users.first_name AS author_of_comment_first_name,
+users.last_name AS author_of_comment_last_name, comments.body AS comment_body, posts.content
+FROM users 
+INNER JOIN posts ON posts.users_id = users.id 
+  INNER JOIN comments ON commetns.posts_id = posts.id 
+    Where comments.body like '%SSL%' AND posts.content LIKE '%dolorum%';
+
+-- 1. Create a query to get the first name of the author of the post (aliased to post_author_first_name), last name of the author of the post (aliased to post_author_last_name), the post title (aliased to post_title), username of the author of the comment (aliased to comment_author_username), and comment body (aliased to comment_body), where the comment body contains the word 'SSL' or 'firewall' and the post content contains the word 'nemo' ( should have 218 results )
+SELECT users.first_name AS post_author_first_name, users.last_name AS post_author_last_name,
+posts.title AS post_title, users.username AS comment_author_username,
+comments.body AS comment_body FROM users_id
+INNER JOIN posts ON posts.users_id = users.id 
+  INNER JOIN comments ON comments.posts_id = posts.id
+    WHERE (comments.body LIKE '%SSL%' OR comments.body like '%firewall%')
+      AND posts.content LIKE '%NEMO%';
 
 
 
